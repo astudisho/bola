22ffce2752be35311f53548551fbcb414d7fa4bd
Javier Astudillo <javier_astudillo@live.com.mx>
2020-12-30 19:43:53 -0600

    Carousel -Carousel working -Added assets -Added nav bar -Added icon for navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CarrouselComponent } from './components/carrousel/carrousel.component';
 import { CarrouselConfig } from './models/carrousel-config';
+import { SelectedYear } from './models/enums/carrousel-year.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'bola-app';
-  firstList = ["chokimi", "chimoso", "chokimi2", "popito-chimi-osos", "popito-cod","popito-oso", "popito"];
+  list2020 = ["chokimi", "chimoso", "chokimi2", "popito-chimi-osos", "popito-cod","popito-oso", "popito"];
+  list2019 = [];
+  list2018 = [];
+  list2017 = [];
 
-  firstCarouselConfig : CarrouselConfig = {
-    sourceList: this.firstList,
+  YearsEnum = SelectedYear;
+  selectedYear : SelectedYear = this.YearsEnum.year2020;
+
+  carouselConfig2020 : CarrouselConfig = {
+    sourceList: this.list2020,
     extension: 'jpg',
-    path: 'assets/img/'
+    path: 'assets/img/2020/'
+  }
+  carouselConfig2019 : CarrouselConfig = {
+    sourceList: this.list2019,
+    extension: 'jpg',
+    path: 'assets/img/2019/'
+  }
+  carouselConfig2018 : CarrouselConfig = {
+    sourceList: this.list2018,
+    extension: 'jpg',
+    path: 'assets/img/2018/'
+  }
+  carouselConfig2017 : CarrouselConfig = {
+    sourceList: this.list2017,
+    extension: 'jpg',
+    path: 'assets/img/2017/'
   }
 }
---
+++ src/app/models/enums/carrousel-year.enum.ts
@@ -0,0 +1,6 @@
+export enum SelectedYear {
+    year2020 = 1,
+    year2019,
+    year2018,
+    year2017
+}
